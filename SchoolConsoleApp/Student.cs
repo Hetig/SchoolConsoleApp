b8cc213cb1af01f29a8bee5bc0d4b33371e09a2d
Хетаг Хачиров <kh.ha4irov2011@yandex.ru>
2022-07-19 21:02:03 +0300

    Вынес классы в отдельные файлы

---
+++ SchoolConsoleApp/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolConsoleApp
+{
+    public class Student
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+
+        public Student(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+    }
+}
